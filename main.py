bc7229019912ab1913a6b8c1e862633d0e2b9845
Joel García <garciajunior796@gmail.com>
2022-01-05 21:42:42 -0500

    Block tweet extraction

--- main.py
+++ main.py
@@ -15,28 +15,28 @@ import recommender_engine.run
 # ----------------------------------------------------------------------------------------
 
 # loading list of keywords
-load_dotenv()
-NUMBER_LISTS = int(os.getenv("NUMBER_LISTS"))  # number of csv files with words
-full_words = []  # this will contain all the words
+# load_dotenv()
+# NUMBER_LISTS = int(os.getenv("NUMBER_LISTS"))  # number of csv files with words
+# full_words = []  # this will contain all the words
 
-assert NUMBER_LISTS > 0  # you need at least one of these files
+# assert NUMBER_LISTS > 0  # you need at least one of these files
 
-# Loading all the words into an unique variable
-for i in range(NUMBER_LISTS):
-    full_words += [str(x[0])
-        for x in (pd.read_csv(f'./Listas/Lista{i + 1}.csv')).values
-    ]
+# # Loading all the words into an unique variable
+# for i in range(NUMBER_LISTS):
+#     full_words += [str(x[0])
+#         for x in (pd.read_csv(f'./Listas/Lista{i + 1}.csv')).values
+#     ]
 # ----------------------------------------------------------------------------------------
 
 
 # how often should the sample of words change
-PERIOD = float(os.getenv("REFRESH_TIME"))
-SAMPLE_SIZE = int(os.getenv("WORD_SAMPLE_SIZE"))
+# PERIOD = float(os.getenv("REFRESH_TIME"))
+# SAMPLE_SIZE = int(os.getenv("WORD_SAMPLE_SIZE"))
 
 # stream_listener = StreamListener(full_words)
 
 # Necessary to stop the filter thread
-loop_thread = None
+# loop_thread = None
 
 recommender_engine.run.train_rbm_model()
 # def event():
